a7f0a3317d249901504195901a7633ef645a4e2e
Finn <finnterdal@gmail.com>
2020-04-23 16:46:28 -0500

    sessions n passport demo

--- server/api/cart.js
+++ server/api/cart.js
@@ -3,19 +3,33 @@ module.exports = router
 const Order = require('../db/models/order')
 const Order_Product = require('../db/models/order_product')
 
-const findByUser = req => {
+const findByUser = (req) => {
   return {
     where: {
       userId: req.params.userId,
-      status: 'created'
+      status: 'created',
     },
-    include: {all: true}
+    include: {all: true},
   }
 }
+
+router.get('/', (req, res, next) => {
+  const user = req.user
+  const session = req.session
+  if (!session.cart) {
+    session.cart = 1
+  } else {
+    session.cart++
+  }
+  console.log('user', user)
+  console.log('session', session)
+  res.json({user, session})
+})
 
 //get items in the cart for the current user
 router.get('/:userId', async (req, res, next) => {
   try {
+    const usersOrders = await req.user.getOrders()
     const createdOrdersById = await Order.findAll(findByUser(req))
     res.json(createdOrdersById)
   } catch (err) {
@@ -36,7 +50,7 @@ router.put('/update-qty/:userId', async (req, res, next) => {
   try {
     const createdOrdersById = await Order.findAll(findByUser(req))
     await createdOrdersById[0].addProduct(req.body.id, {
-      through: {cartQuantity: req.body.order_product.cartQuantity}
+      through: {cartQuantity: req.body.order_product.cartQuantity},
     })
     res.json(createdOrdersById[0])
   } catch (err) {
