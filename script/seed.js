b7f8e73a9f6ec959778b503311454c64c4a65b06
Emily Ackerman <emily.ackerman626@gmail.com>
2020-04-23 11:54:59 -0500

    cleaned up seed file, google auth, and prettier version (should stop seeing the prettier error in the bottom right whenever you save)

--- script/seed.js
+++ script/seed.js
@@ -8,7 +8,7 @@ const {
   Order,
   Review,
   Order_Product,
-  Category
+  Category,
 } = require('../server/db/models')
 
 async function seed() {
@@ -17,7 +17,7 @@ async function seed() {
 
   const users = [
     {email: 'cody@email.com', password: '123'},
-    {email: 'murphy@email.com', password: '123'}
+    {email: 'murphy@email.com', password: '123'},
   ]
 
   const products = [
@@ -25,39 +25,44 @@ async function seed() {
       title: 'Eucalyptus',
       description:
         'has healing properties, koalas eat it, poisonous to most other things',
+      categoryId: 1,
       price: 29.99,
       inventoryQuantity: 20,
       imageUrl:
-        'https://www.parasperfumers.com/upload/product_ecom/Eucalyptus-Radiata-Essential-Oil-ProductPic.jpg'
+        'https://www.parasperfumers.com/upload/product_ecom/Eucalyptus-Radiata-Essential-Oil-ProductPic.jpg',
     },
     {
       title: 'Aloe Vera',
       description: 'good for sunburns, poisonous to cats',
+      categoryId: 4,
       price: 3.5,
       inventoryQuantity: 50,
       imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlzFimnp1eBL0y4FkFNUJgQnWrRh0i4QlDdnP_ZtdjG2FEGBRc&usqp=CAU'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlzFimnp1eBL0y4FkFNUJgQnWrRh0i4QlDdnP_ZtdjG2FEGBRc&usqp=CAU',
     },
     {
       title: 'Oak Seedling',
       description: 'will grow into a big tree, produces acorns',
+      categoryId: 1,
       price: 10.0,
-      inventoryQuantity: 8
+      inventoryQuantity: 8,
     },
     {
       title: 'Tulip',
       description: 'pretty basic flower',
+      categoryId: 2,
       price: 2.0,
-      inventoryQuantity: 45
+      inventoryQuantity: 1, // someone has >1 of these in their cart
     },
     {
       title: 'Purple Passion Vine',
       description: 'fuzzy and purple',
+      categoryId: 5,
       price: 9.5,
       inventoryQuantity: 23,
       imageUrl:
-        'https://www.netclipart.com/pp/m/346-3467700_clip-art-purple-branch-purple-leaves-clip-art.png'
-    }
+        'https://www.netclipart.com/pp/m/346-3467700_clip-art-purple-branch-purple-leaves-clip-art.png',
+    },
   ]
 
   const categories = [
@@ -65,74 +70,52 @@ async function seed() {
     {name: 'flower'},
     {name: 'shrub'},
     {name: 'succulent'},
-    {name: 'vine'}
+    {name: 'vine'},
   ]
 
-  const orders = [
-    {status: 'processing'},
-    {status: 'created'},
-    {status: 'complete'},
-    {status: 'complete'},
-    {status: 'cancelled'}
-  ]
   const orderProducts = [
     {orderId: 1, productId: 1, cartQuantity: 1, checkoutPrice: 19.99},
-    {orderId: 2, productId: 2, cartQuantity: 2, checkoutPrice: 29.99},
-    {orderId: 3, productId: 3, cartQuantity: 1, checkoutPrice: 39.99},
-    {orderId: 4, productId: 4, cartQuantity: 3, checkoutPrice: 15.99},
-    {orderId: 5, productId: 5, cartQuantity: 1, checkoutPrice: 29.99}
+    {orderId: 1, productId: 2, cartQuantity: 2, checkoutPrice: 2.0},
+    {orderId: 1, productId: 3, cartQuantity: 4, checkoutPrice: 3.5},
+    {orderId: 1, productId: 4, cartQuantity: 1, checkoutPrice: 10.89},
+    {orderId: 2, productId: 2, cartQuantity: 2}, // not checked out yet
+    {orderId: 2, productId: 4, cartQuantity: 3}, // not checked out yet
+    {orderId: 3, productId: 3, cartQuantity: 20, checkoutPrice: 9.99},
+    {orderId: 4, productId: 4, cartQuantity: 3, checkoutPrice: 29.99},
+    {orderId: 4, productId: 2, cartQuantity: 1, checkoutPrice: 10.0},
+    {orderId: 5, productId: 5, cartQuantity: 1}, // not checked out yet
+    // orderId: 6 has no items in the cart, so they have no rows in this table
   ]
   const reviews = [
-    {description: 'Loved my aloe vera! 10/10 would recommend'},
-    {description: 'I killed my purple passion vine'},
-    {description: "My oak tree isn't growing fast enough"},
-    {description: 'Aloe vera tastes funny'}
+    {
+      description: 'Loved my aloe vera! 10/10 would recommend',
+      userId: 1,
+      productId: 2,
+    },
+    {description: 'I killed my purple passion vine', userId: 1, productId: 5},
+    {
+      description: "My oak tree isn't growing fast enough",
+      userId: 2,
+      productId: 3,
+    },
+    {description: 'Aloe vera tastes funny', userId: 2, productId: 2},
   ]
-  console.log('magic methods', Object.keys(User.prototype))
 
   const orders = [
-    {status: 'processing'},
+    {status: 'processing', userId: 1},
     {status: 'created', userId: 1},
-    {status: 'complete'},
-    {status: 'complete'},
-    {status: 'cancelled'}
-  ]
-  const orderProducts = [
-    {orderId: 1, productId: 1, cartQuantity: 1, checkoutPrice: 19.99},
-    {orderId: 2, productId: 2, cartQuantity: 2, checkoutPrice: 29.99},
-    {orderId: 2, productId: 1, cartQuantity: 1, checkoutPrice: 19.99},
-    {orderId: 3, productId: 3, cartQuantity: 1, checkoutPrice: 39.99},
-    {orderId: 4, productId: 4, cartQuantity: 3, checkoutPrice: 15.99},
-    {orderId: 5, productId: 5, cartQuantity: 1, checkoutPrice: 29.99}
+    {status: 'complete', userId: 1},
+    {status: 'complete', userId: 2},
+    {status: 'cancelled', userId: 1},
+    {status: 'created', userId: 2}, // this order was created, but then the user deleted all items from their cart
   ]
 
   await User.bulkCreate(users)
-  await Product.bulkCreate(products)
   await Category.bulkCreate(categories)
+  await Product.bulkCreate(products)
   await Order.bulkCreate(orders)
   await Order_Product.bulkCreate(orderProducts)
-
-  const eucalyptus = await Product.findByPk(1)
-  await eucalyptus.setCategory(1)
-  const aloe = await Product.findByPk(2)
-  await aloe.setCategory(4)
-  const oak = await Product.findByPk(3)
-  await oak.setCategory(1)
-  const tulip = await Product.findByPk(4)
-  await tulip.setCategory(2)
-  const purple = await Product.findByPk(5)
-  await purple.setCategory(5)
-
-  const cody = await User.findByPk(1)
-  const aloeReview1 = await cody.createReview(reviews[0])
-  await aloeReview1.setProduct(2)
-  const purpReview = await cody.createReview(reviews[1])
-  await purpReview.setProduct(5)
-  const murphy = await User.findByPk(2)
-  const oakReview = await murphy.createReview(reviews[2])
-  await oakReview.setProduct(3)
-  const aloeReview2 = await murphy.createReview(reviews[3])
-  await aloeReview2.setProduct(2)
+  await Review.bulkCreate(reviews)
 
   console.log(`seeded successfully`)
 }
